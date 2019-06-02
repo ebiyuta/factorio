c17d060ee62ac34203b43f09592efcbc412d40bc
ebiyuta <xxpino123pinoxx@gmail.com>
2019-06-02 18:42:50 +0900

    first Commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,126 @@
+module.exports = {
+  title: '惑星探索日記',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico'}],
+    ['meta',{ name:"keywords", content:"vuepress, netlify, えび"}],
+    ['meta',{ name:"og:title", content:"惑星探索日記"}],
+    ['meta',{ name:"og:description", content:"惑星探索日記"}],
+    ['meta',{ name:"og:type", content:"website"}],
+    ['meta',{ name:"og:url", content:"og　url"}]
+  ],
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: '惑星探索日記',
+      description: '惑星探索日記',
+    }
+  },
+ themeConfig: {
+    nav: [
+    ],
+    sidebar: [
+      {
+        title: '惑星探索日記まとめ',
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: '惑星探索日記0日目',
+        children: [
+          '/day00/'
+        ]
+      },
+      {
+        title: '惑星探索日記1日目',
+        children: [
+          '/day01/'
+        ]
+      },
+      {
+        title: '惑星探索日記2日目',
+        children: [
+          '/day02/'
+        ]
+      },
+      {
+        title: '惑星探索日記3日目',
+        children: [
+          '/day03/'
+        ]
+      },
+      {
+        title: '惑星探索日記4日目',
+        children: [
+          '/day04/'
+        ]
+      },
+      {
+        title: '惑星探索日記5日目',
+        children: [
+          '/day05/'
+        ]
+      },
+      {
+        title: '惑星探索日記6日目',
+        children: [
+          '/day06/'
+        ]
+      },
+      {
+        title: '惑星探索日記7日目',
+        children: [
+          '/day07/'
+        ]
+      },
+      {
+        title: '惑星探索日記8日目',
+        children: [
+          '/day08/'
+        ]
+      },
+      {
+        title: '惑星探索日記9日目',
+        children: [
+          '/day09/'
+        ]
+      },
+      {
+        title: '惑星探索日記10日目',
+        children: [
+          '/day10/'
+        ]
+      },
+      {
+        title: '惑星探索日記11日目',
+        children: [
+          '/day11/'
+        ]
+      },
+      {
+        title: '惑星探索日記12日目',
+        children: [
+          '/day12/'
+        ]
+      },
+      {
+        title: '惑星探索日記13日目',
+        children: [
+          '/day13/'
+        ]
+      },
+      {
+        title: '惑星探索日記14日目',
+        children: [
+          '/day14/'
+        ]
+      },
+      {
+        title: '惑星探索日記おまけ',
+        children: [
+          '/day15/'
+        ]
+      }
+    ]
+  }
+}
